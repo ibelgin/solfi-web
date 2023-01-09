1acac0ec50059f36e36c484db1121e202a547309
Belgin <61349423+ibelgin@users.noreply.github.com>
2023-01-10 00:24:28 +0530

    Added Routers and Navigators

---
+++ src/routes/Home/index.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import "./styles.css";
+
+const MainPage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Home Screen</p>
+      </header>
+    </div>
+  );
+};
+
+export default MainPage;
