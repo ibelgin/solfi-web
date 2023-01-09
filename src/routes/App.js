32e9d5df3d93023e08ae78480032aae0370f2ec9
Belgin <61349423+ibelgin@users.noreply.github.com>
2023-01-10 01:14:16 +0530

    Done

--- src/routes/App.js
+++ src/routes/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/SolFi-Web' element={<Home/>} />
           <Route path='/about' element={<About />} />
         </Routes>
     </>
